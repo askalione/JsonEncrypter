588997f65d1737ed196f5a31376b1f4f8f578142
Alexey Drapash <godhot@yandex.ru>
2020-04-22 00:32:40 +0300

    Upd

--- src/JsonEncrypter/Program.cs
+++ src/JsonEncrypter/Program.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json;
+using Structr.Security;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,66 +10,49 @@ namespace JsonEncrypter
 {
     class Program
     {
+        private const string _resultSuffix = ".encrypted";
+
         static void Main(string[] args)
         {
-            string sourcePath = GetPath(args);
-            if (string.IsNullOrEmpty(sourcePath))
+            try
             {
-                sourcePath = Path.Combine(Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().Location).LocalPath), "source.json");
-            }
-            if (!File.Exists(sourcePath))
-            {
-                Console.WriteLine("Source file not found. Use source.json or -path argument.");
-                return;
-            }
-            Console.WriteLine($"Detect source: {sourcePath}");            
+                Console.Write("Path: ");
+                var sourcePath = Console.ReadLine();
+                if (string.IsNullOrEmpty(sourcePath))
+                    throw new InvalidOperationException("Source path is required.");
+                if (File.Exists(sourcePath) == false)
+                    throw new InvalidOperationException("Source file not found.");
 
-            var items = JsonConvert.DeserializeObject<Dictionary<string, EncryptingItem>>(File.ReadAllText(sourcePath));
-            Console.WriteLine("Encrypting...");
-            items = Encrypt(items);
+                Console.Write("Passphrase: ");
+                var passphrase = Console.ReadLine();
+                if (string.IsNullOrEmpty(passphrase))
+                    throw new InvalidOperationException("Passphrase is required.");
 
-            var dir = Path.GetDirectoryName(sourcePath);
-            var sourceFileName = Path.GetFileNameWithoutExtension(sourcePath);
-            var sourceExt = Path.GetExtension(sourcePath);
-            var resultFileName = sourceFileName + ".encrypted" + sourceExt;
-            var resultPath = Path.Combine(dir, resultFileName);
-            var result = JsonConvert.SerializeObject(items, Formatting.Indented);
+                var source = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(sourcePath));
 
-            Console.WriteLine("Save encrypted result...");
-            File.WriteAllText(resultPath, result, Encoding.UTF8);
-        }
-
-        private static string GetPath(string[] args)
-        {
-            for (int i = 0; i < args.Length; i++)
-            {
-                if (args[i] == "-path" && i != args.Length - 1)
+                var encrypted = new Dictionary<string, string>();
+                foreach (var key in source.Keys)
                 {
-                    return args[i + 1]?.Trim();
+                    var value = source[key];
+                    encrypted[key] = !string.IsNullOrEmpty(value)
+                        ? StringEncoder.Encrypt(value, passphrase)
+                        : value;
                 }
+
+                var dir = Path.GetDirectoryName(sourcePath);
+                var sourceFileName = Path.GetFileNameWithoutExtension(sourcePath);
+                var sourceExt = Path.GetExtension(sourcePath);
+                var resultFileName = sourceFileName + _resultSuffix + sourceExt;
+                var resultPath = Path.Combine(dir, resultFileName);
+                var result = JsonConvert.SerializeObject(encrypted, Formatting.Indented);
+
+                File.WriteAllText(resultPath, result, Encoding.UTF8);
             }
-
-            return null;
-        }
-
-        private static Dictionary<string, EncryptingItem> Encrypt(Dictionary<string, EncryptingItem> items)
-        {
-            var encrypters = new Dictionary<string, Encrypter>();
-            
-
-            foreach(var name in items.Keys)
+            catch(Exception ex)
             {
-                var item = items[name];
-                if (!string.IsNullOrEmpty(item.Key) && !string.IsNullOrEmpty(item.Value))
-                {
-                    if (!encrypters.ContainsKey(item.Key))
-                        encrypters.Add(item.Key, new Encrypter(item.Key));
-                    var encrypter = encrypters[item.Key];
-                    item.Value = encrypter.Encrypt(item.Value);
-                }
+                Console.WriteLine($"Failure: {ex.Message}");
+                Console.ReadKey();
             }
-
-            return items;
         }
     }
 }
